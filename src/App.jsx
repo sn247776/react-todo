5ed223ae90d37a8cc3e896871ce80dd0b9e45f1f
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-04-04 23:31:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./components/TopBar";
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Router>
+      <Topbar/>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+    </Router>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
+}
+
+export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+const Login = () => {
+  return (
+    <Box className='login-page'>
+      <h1>Login</h1>
+    </Box>
+  )
+}
+
+export default Login
