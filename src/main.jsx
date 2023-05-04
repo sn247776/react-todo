aafc34317e54c4f393fd5a493d1af71ecbc5da2c
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-05-04 16:27:15 +0530

    finall done

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 // import "./styles/app.scss";
 import { createContext } from "react";
 
-export const server = "todo-backend-dy86.onrender.com/api/v1";
+export const server = "https://todo-backend-dy86.onrender.com/api/v1";
 
 export const Context = createContext({ isAuthenticated: false });
 
